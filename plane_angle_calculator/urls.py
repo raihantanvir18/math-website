905b0a86c95544fcf47e2910e1cb2293fbc0a7b3
Raihan <raihantanvir118@gmail.com>
2023-10-12 18:13:19 +0600

    changed for vercel

--- plane_angle_calculator/urls.py
+++ plane_angle_calculator/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+import static
+import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('calculator.urls')),
     
 ]
+
+urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
